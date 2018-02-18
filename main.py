bce49be148571caf19a4001dbdd36e9410dafe09
levifussell <levifussell@hotmail.com>
2018-02-18 14:53:02 +0000

    change error metric to RMSE

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # because ESN are stochastic, this will train a ESN multiple times and compute the average MSE
-def ESN_stochastic_train(data, train_split, esn, num_runs, seed=None):
+def ESN_stochastic_train(data, train_split, esn, num_runs, MEAN_OF_DATA, seed=None):
 
     data_train = data[:train_split]
     #print(np.shape(data_train))
-    input_size = 1
+    input_size = esn.input_size - 1
     data_train_X = np.zeros((np.shape(data_train)[0] - input_size, input_size))
     data_train_y = np.zeros((np.shape(data_train)[0] - input_size, 1))
     #print(np.shape(data_train_X))
@@ -46,30 +46,37 @@ def ESN_stochastic_train(data, train_split, esn, num_runs, seed=None):
 
     mean_mse_test = 0
     mean_mse_train = 0
-    #for e in range(num_runs):
-    #esn_copy = esn.copy()
-    esn.train(data_train_X, data_train_y)
+    mean_gen_test = 0
+    mean_gen_train = 0
+    for e in range(num_runs):
+        esn_copy = esn.copy()
+        esn_copy.train(data_train_X, data_train_y)
 
-    if esn.debug:
-        plt.bar(range(esn.input_size+esn.reservoir_size), esn.W_out)
-        plt.title("Output Weights")
-        plt.show()
+        if esn_copy.debug:
+            plt.bar(range(esn_copy.input_size+esn.reservoir_size), esn_copy.W_out)
+            plt.title("Output Weights")
+            plt.show()
 
-    y_pred_test = esn.predict(data_test_X)
-    # plt.plot(range(len(y_pred_test)), y_pred_test)
-    # plt.plot(range(len(data_test_X)), data_test_X)
-    # plt.show()
-    y_pred_train = esn.predict(data_train_X, reset_res=True)
+        y_pred_test = esn_copy.predict(data_test_X)
+        # plt.plot(range(len(y_pred_test)), y_pred_test)
+        # plt.plot(range(len(data_test_X)), data_test_X)
+        # plt.show()
+        y_pred_train = esn_copy.predict(data_train_X, reset_res=True)
 
-    mse_test = esn.mean_l2_error(data_test_y, y_pred_test)
-    mse_train = esn.mean_l2_error(data_train_y[esn.init_echo_timesteps:], y_pred_train)
-    mean_mse_test += mse_test
-    mean_mse_train += mse_train
-    #print("iter: {} -- Mean L2 Error TEST: {}, TRAIN: {}".format(e, mse_test, mse_train))
-    #print("iter: {} -- Mean L2 Error TEST: {}".format(e, mse_test))
+        mse_test = esn_copy.nmse(data_test_y, y_pred_test, MEAN_OF_DATA)
+        mse_train = esn_copy.nmse(data_train_y[esn.init_echo_timesteps:], y_pred_train, MEAN_OF_DATA)
+        mean_mse_test += mse_test
+        mean_mse_train += mse_train
 
     #gen_err, generated_data = esn_copy.generate(data_test[:1000], plot=False)
-    gen_err, generated_data = esn.generate(data_test[:1500], plot=False)
+        gen_err_test, generated_data_test = esn_copy.generate(data_test[:2000], MEAN_OF_DATA, plot=False, show_error=False)
+        gen_err_train, generated_data_train = esn_copy.generate(data_train[:2000], MEAN_OF_DATA, plot=False, show_error=False)
+
+        mean_gen_test += gen_err_test
+        mean_gen_train += gen_err_train
+
+        print("iter: {} -- NRMSE (SUP) Error TEST: {}, TRAIN: {}".format(e, mse_test, mse_train))
+        print("iter: {} -- NRMSE (GEN) Error TEST: {}, TRAIN: {}".format(e, mean_gen_test, mean_gen_train))
 
     #g_data = [generated_data, generated_data, generated_data, generated_data]
     #d = data_test[(esn_copy.init_echo_timesteps+esn_copy.input_size-1):1000, 0]
@@ -78,9 +85,13 @@ def ESN_stochastic_train(data, train_split, esn, num_runs, seed=None):
 
     mean_mse_test /= num_runs
     mean_mse_train /= num_runs
-    print("\n\nFINAL -- Mean L2 Error TEST: {}, TRAIN: {}".format(mean_mse_test, mean_mse_train))
+    mean_gen_train /= num_runs
+    mean_gen_test /= num_runs
 
-    return gen_err, generated_data, data_test[(esn.init_echo_timesteps+esn.input_size-1):1500,0], esn.training_signals
+    print("\n\nFINAL -- Supervised Mean L2 Error TEST: {}, TRAIN: {}".format(mean_mse_test, mean_mse_train))
+    print("\n\nFINAL -- Generative Mean L2 Error TEST: {}, TRAIN: {}".format(mean_gen_test, mean_gen_train))
+
+    return gen_err_test, generated_data_test, data_test[(esn_copy.init_echo_timesteps+esn.input_size-1):2000,0], esn_copy.training_signals
 
 def fancy_plot_generative(generated_data, actual_data, num_rows, num_cols, titles=[]):
     # generated data and actual data should be lists of data sets so that subplots can be made
@@ -122,7 +133,7 @@ def fancy_plot_signals(signals_data, num_rows, num_cols, titles=[]):
 
 
 if __name__ == "__main__":
-    data = np.array([run(12000)]).T
+    data = np.array([run(20000)]).T
     #data = np.loadtxt('../../../MackeyGlass_t17.txt')
     #data -= np.mean(data)
     #data += 100.0
@@ -130,9 +141,11 @@ if __name__ == "__main__":
     #print(np.std(data))
     #data /= np.std(data)
     #data *= 2.0
-    onExit(data)
+    # onExit(data)
     #esn = ESN(input_size=2, output_size=1, reservoir_size=1000, echo_param=0.1, spectral_scale=1.1, init_echo_timesteps=100, regulariser=1e-0, debug_mode=True)
     #ESN_stochastic_train(data, 7000, esn, 1)
+    MEAN_OF_DATA = np.mean(data)
+    print("DATA MEAN: {}".format(MEAN_OF_DATA))
 
     g_data = []
     a_data = []
@@ -146,8 +159,9 @@ if __name__ == "__main__":
                 for s in [1.2, 1.6]:
                     for a in [1/1.]:
                         print("EXPERIMENT - ECHO {}, RES {}, REG {}, SPECT {}, W_IN {}".format(e, r, reg, s, a))
-                        esn = ESN(input_size=2, output_size=1, reservoir_size=r, echo_param=e, spectral_scale=s, init_echo_timesteps=100,regulariser=reg, input_weights_scale=a, debug_mode=False)
-                        gen_err, g_, a_, signals_ = ESN_stochastic_train(data, 7000, esn, 1)
+                        esn = ESN(input_size=2, output_size=1, reservoir_size=r, echo_param=e, spectral_scale=s, 
+                                    init_echo_timesteps=100,regulariser=reg, input_weights_scale=a, debug_mode=False)
+                        gen_err, g_, a_, signals_ = ESN_stochastic_train(data, 14000, esn, 1, MEAN_OF_DATA)
                         g_data.append(g_)
                         a_data.append(a_)
                         titles.append(("ECHO: {:.2f}, SPEC: {:.2f}, REG: {}, W-IN: {}".format(e, s, reg, a)))
