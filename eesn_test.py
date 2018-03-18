e5fe7ab47cf3edd47bdfd7e4dc6c19a9d05888d3
Levi Fussell <s1408726@bruegel.inf.ed.ac.uk>
2018-03-18 11:03:47 +0000

    run ESN test

--- eesn_test.py
+++ eesn_test.py
@@ -82,7 +82,8 @@ if __name__ == '__main__':
 
     data_samples = np.zeros((len(res_ranges)*len(echo_ranges)*len(spect_ranges)*len(res_number_ranges)*len(weightin_ranges), 1+2+2+2+1+1))
     data_csv = np.zeros((len(res_ranges)*len(echo_ranges)*len(spect_ranges)*len(res_number_ranges)*len(weightin_ranges), 1+2+2+2+1+1))
-    
+    runs = 200 
+    nrmses_d = []
     idx = 0
     for n in res_number_ranges:
         for r in res_ranges:
@@ -116,46 +117,56 @@ if __name__ == '__main__':
                                     #strategies=['uniform']*n,
                                     #sparsity=0.1
                                     #)
-                        eesn = ESN(1, 1, reservoir_size=1000,
-                                    echo_param=0.85,
-                                    regulariser=1e-5, debug=True,
-                                    # activation=(lambda x: x*(x>0).astype(float)),
-                                    # activation=(lambda x: x),
-                                    init_echo_timesteps=100)
-                                    # init_echo_timesteps=100, dims_reduce=(np.linspace(50, 200, n-1).astype(int).tolist()),
-                        eesn.initialize_input_weights(scale=1.0)
-                        #eesn.reservoir.W_in[:, -1] += MEAN_OF_DATA
-                        eesn.initialize_reservoir_weights(
-                                    spectral_scale=1.25,
-                                    sparsity=1.0)
-                        eesn.train(X_train, y_train)
+                        for i in range(runs):
+                            eesn = ESN(1, 1, reservoir_size=1000,
+                                        echo_param=0.85,
+                                        regulariser=1e-5, debug=True,
+                                        # activation=(lambda x: x*(x>0).astype(float)),
+                                        # activation=(lambda x: x),
+                                        init_echo_timesteps=100)
+                                        # init_echo_timesteps=100, dims_reduce=(np.linspace(50, 200, n-1).astype(int).tolist()),
+                            eesn.initialize_input_weights(scale=1.0)
+                            #eesn.reservoir.W_in[:, -1] += MEAN_OF_DATA
+                            eesn.initialize_reservoir_weights(
+                                        spectral_scale=1.25,
+                                        sparsity=1.0)
+                            eesn.train(X_train, y_train)
 
-                        eesn_outputs = []
+                            eesn_outputs = []
 
-                        # GENERATIVE =================================================
-                        u_n_EESN = data[split-1]
-                        for _ in range(len(data[split:])):
-                            u_n_EESN = eesn.forward(u_n_EESN)
-                            eesn_outputs.append(u_n_EESN)
+                            # GENERATIVE =================================================
+                            u_n_EESN = data[split-1]
+                            for _ in range(len(data[split:])):
+                                u_n_EESN = eesn.forward(u_n_EESN)
+                                eesn_outputs.append(u_n_EESN)
 
-                        # SUPERVISED ====
-                        # for u_n in X_valid:
-                        #     esn_outputs.append(esn.forward(u_n))
-                        #     lcesn_outputs.append(lcesn.forward(u_n))
-                        # ============================================================
+                            # SUPERVISED ====
+                            # for u_n in X_valid:
+                            #     esn_outputs.append(esn.forward(u_n))
+                            #     lcesn_outputs.append(lcesn.forward(u_n))
+                            # ============================================================
 
-                        # esn_outputs = np.array(esn_outputs).squeeze()
-                        eesn_outputs = np.array(eesn_outputs).squeeze()
-                        y_vals = y_valid.squeeze()
-                        # print(np.shape(eesn_outputs))
-                        # print(np.shape(y_vals))
-                        # print(np.vstack((eesn_outputs, y_vals)))
+                            # esn_outputs = np.array(esn_outputs).squeeze()
+                            eesn_outputs = np.array(eesn_outputs).squeeze()
+                            y_vals = y_valid.squeeze()
+                            # print(np.shape(eesn_outputs))
+                            # print(np.shape(y_vals))
+                            # print(np.vstack((eesn_outputs, y_vals)))
 
-                        # print('  ESN MSE: %f' % mse(y_valid, esn_outputs))
-                        # print('EESN MSE: %f' % mse(y_valid, eesn_outputs))
-                        nrmse_err = nrmse(y_vals, eesn_outputs, MEAN_OF_DATA)
-                        print('DHESN NRMSE: %f' % nrmse_err)
+                            # print('  ESN MSE: %f' % mse(y_valid, esn_outputs))
+                            # print('EESN MSE: %f' % mse(y_valid, eesn_outputs))
+                            nrmse_err = nrmse(y_vals, eesn_outputs, MEAN_OF_DATA)
+                            print('DHESN NRMSE: %f' % nrmse_err)
+                            nrmses_d.append(nrmse_err)
 
+                        import pickle as pkl
+                        pkl.dump(nrmses_d, open("ESN_hist_data2.pkl", "wb"))
+                        hist, bins = np.histogram(nrmses_d, bins=50)
+                        centres = (bins[1:] + bins[:-1]) / 2.
+                        width = bins[1] - bins[0]
+                        plt.bar(centres, hist, width=width)
+                        plt.show()
+    
                         plt.plot(range(len(eesn_outputs)), eesn_outputs, label="predicted")
                         plt.plot(range(len(y_vals)), y_vals, label="true")
                         plt.legend()
